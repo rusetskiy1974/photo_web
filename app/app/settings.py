32315e8da4fdfaa04f68966744dc6d3949d29530
RSA <113336060+rusetskiy1974@users.noreply.github.com>
2025-05-28 11:36:01 +0200

    fix database koyeb

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,6 +22,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.meta.ua'
 EMAIL_PORT = 465
@@ -141,16 +142,42 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'photo_web',
+#         'USER': 'postgres',
+#         'PASSWORD': '567234',
+#         'HOST': 'postgres',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'photo_web',
-        'USER': 'postgres',
-        'PASSWORD': '567234',
-        'HOST': 'postgres',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('PORT'),
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'koyebdb',
+#         'USER': 'koyeb-adm',
+#         'PASSWORD': 'npg_m7nKYPBl1sTq',
+#         'HOST': 'ep-shiny-base-a20i2aiy.eu-central-1.pg.koyeb.app',
+#         'PORT': '5432',
+#         'OPTIONS': {'sslmode': 'require'},
+#     }
+# }
+
+
 
 
 # Password validation
